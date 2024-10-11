25d15da6dceeb7e6672e3acebac546750d811407
LifeHckr <jarodthereal@gmail.com>
2024-10-11 16:43:44 -0700

    Step 1: Added h1, and canvas. Added css styling for canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
+const APP_NAME : string = "Draw Thing Please!";
+const app: HTMLDivElement  = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const title :HTMLHeadingElement =  document.createElement("h1");
+title.innerHTML = APP_NAME;
+app.append(title);
+const CANVAS_WIDTH : number = 256
+const CANVAS_HEIGHT : number = 256
+const canvas = document.createElement("canvas");
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+app.append(canvas);
+const ctx = canvas.getContext("2d");
