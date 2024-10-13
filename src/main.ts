c341215a8dc49c1daed0d330a49cfd2438874a15
LifeHckr <jarodthereal@gmail.com>
2024-10-13 09:50:00 -0700

    Step 4: complete I don't need a redo and undo buffer.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import "./style.css";
 
 //const's-----------------------------------
 const APP_NAME : string = "Draw Thing Please!";
-const CANVAS_WIDTH : number = 256
-const CANVAS_HEIGHT : number = 256
+const CANVAS_WIDTH : number = 256;
+const CANVAS_HEIGHT : number = 256;
 const drawing_changed : Event = new CustomEvent("drawing-changed");
 //-------end const's---------------------------------
 //HTML SETUP------------------------------------
@@ -17,10 +17,18 @@ canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 page.append(canvas);
 const button_div : HTMLDivElement = document.createElement("div");
-page.append(button_div)
-const clearButton = document.createElement("button");
-clearButton.innerHTML = "clear";
-button_div.append(clearButton);
+page.append(button_div);
+const command_button_div : HTMLDivElement = document.createElement("div");
+button_div.append(command_button_div);
+const undo_button = document.createElement("button");
+undo_button.innerHTML = "undo";
+command_button_div.append(undo_button);
+const redo_button = document.createElement("button");
+redo_button.innerHTML = "redo";
+command_button_div.append(redo_button);
+const clear_button = document.createElement("button");
+clear_button.innerHTML = "clear";
+command_button_div.append(clear_button);
 const ctx = canvas.getContext("2d");
 //END HTML-------------------------------------------
 
@@ -29,7 +37,9 @@ type stroke = [start_x:number, start_y:number, end_x:number, end_y:number];
 type path = stroke[];
 
 let draw_buffer : path[] = [];
-let buffer_size : number = 0;
+let draw_buffer_size : number = 0;
+let undo_buffer : path[] = [];
+let undo_buffer_size : number = 0;
 
 const cursor = { active: false, x: 0, y: 0 };
 
@@ -49,23 +59,39 @@ function draw_all_to_canvas(ctx : CanvasRenderingContext2D, buffer : path[]) {
 }
 function clear_canvas(ctx : CanvasRenderingContext2D) {
     draw_buffer = [];
-    buffer_size = 0;
+    draw_buffer_size = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
+function undo() {
+    if (draw_buffer_size > 0) {
+        undo_buffer.push(draw_buffer.pop());
+        draw_buffer_size--;
+        undo_buffer_size++;
+    }
+    draw_all_to_canvas(ctx, draw_buffer);
+}
+function redo() {
+    if (undo_buffer_size > 0) {
+        draw_buffer.push(undo_buffer.pop());
+        draw_buffer_size++;
+        undo_buffer_size--;
+    }
+    draw_all_to_canvas(ctx, draw_buffer);
+}
 canvas.addEventListener("mousedown", (e) => {
     if (cursor.active) { //temporary fix for issues arising when the cursor leaves the canvas
-        buffer_size++;
+        draw_buffer_size++;
+        undo_buffer_size = 0; //reset undo buffer if an action has been taken, cant undo the draw then redo
     }
     cursor.active = true;
-    draw_buffer[buffer_size] = [];
+    draw_buffer[draw_buffer_size] = [];
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
 });
 canvas.addEventListener("mousemove", (e) => {
     if (cursor.active) {
         let cur_stroke : stroke = [cursor.x, cursor.y, e.offsetX, e.offsetY];
-        draw_buffer[buffer_size].push(cur_stroke);
+        draw_buffer[draw_buffer_size].push(cur_stroke);
         canvas.dispatchEvent(drawing_changed);
         cursor.x = e.offsetX;
         cursor.y = e.offsetY;
@@ -73,14 +99,21 @@ canvas.addEventListener("mousemove", (e) => {
 });
 canvas.addEventListener("mouseup", () => {
     cursor.active = false;
-    buffer_size++;
+    draw_buffer_size++;
+    undo_buffer_size = 0;
 });
 canvas.addEventListener("drawing-changed", () => {
     draw_all_to_canvas(ctx, draw_buffer);
 });
-
-clearButton.addEventListener("click", () => {
+undo_button.addEventListener("click", () => {
+    undo();
+});
+redo_button.addEventListener("click", () => {
+    redo();
+});
+clear_button.addEventListener("click", () => {
     clear_canvas(ctx);
+    undo_buffer_size = 0;
 });
 
 
